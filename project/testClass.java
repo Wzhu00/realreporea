fc4f8dcc39cc0812136b5d05ee49177f47420eb4
Artsiom Balakir <abalaki6@students.d125.org>
2016-03-03 15:21:23 -0600

    mess

---
+++ project/testClass.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class testClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class testClass
+{
+    public void main()
+    {
+        System.out.println("Message 5+5=10");
+    }
+}
