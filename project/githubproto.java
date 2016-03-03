773289a872c5ce2dd35f0875b9c5484693f2cb7e
wilson <wzhu8@students.d125.org>
2016-03-03 15:22:54 -0600

    added everything

---
+++ project/githubproto.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class githubproto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class githubproto
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public static void main (String [] args){
+        System.out.println("Hi art, I am stupid");
+    }
+}
